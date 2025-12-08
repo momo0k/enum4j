7bad96fa10edd5d5a77eaa4c57b3051bbc490b3b
Malve Loren Amber <92647790+maloam@users.noreply.github.com>
2025-12-08 10:52:35 +0100

    Enable strict mode for error handling in script
    Added strict error handling to the script.

--- rpcclient_bruteforce.sh
+++ rpcclient_bruteforce.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+# Fail fast and be strict about errors
+# set -e : exit on nonzero exit status
+# set -u : exit on unset variable reference
+# set -o pipefail : prevent pipeline errors from being masked
+# (See http://redsymbol.net/articles/unofficial-bash-strict-mode/)
 set -euo pipefail
 
 TARGET=''
 LOGFILE='rpcclient_bruteforce.log'
 ts=$(date -Is)
 
+set +e
 printf '%s RID - queryuser:\n' "$ts" >> "$LOGFILE"
 for i in $(seq 500 1100);do rpcclient -N -U "" "$TARGET" -c "queryuser 0x$(printf '%x\n' $i)" >> "$LOGFILE" 2>&1 ;done
+# set -e
