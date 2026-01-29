f35de2359b25d7a9d4b1735cb8ff88c7e62fab14
𒈟𑨻 <92647790+maloam@users.noreply.github.com>
2026-01-29 14:43:20 +0100

    document IMAPS command usage with tags
    added additional tags for IMAPS commands

--- popimaps.sh
+++ popimaps.sh
@@ -7,6 +7,9 @@ echo "== IMAPS scan: $(date -u +'%Y-%m-%dT%H:%M:%SZ')" >> "$LOGFILE"
 openssl s_client -connect "${TARGET}:imaps" -crlf 2>&1 >> "$LOGFILE"
 # u may want to check curl -k 'imaps://10.10.10.10' --user name:pw -v
 # tag0 LOGIN name pw
+# tag1 LIST "" "*"
+# tag2 SELECT xyz
+# tag3 FETCH 1 (BODY[])
 
 echo -e "\n== POP3S scan: $(date -u +'%Y-%m-%dT%H:%M:%SZ')" >> "$LOGFILE"
 openssl s_client -connect "${TARGET}:pop3s" -crlf 2>&1 >> "$LOGFILE"
