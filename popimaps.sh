b5951bd6fac7f82719cd1d8c419ddde5454863ea
𒈟𑨻 <92647790+maloam@users.noreply.github.com>
2026-01-29 14:27:46 +0100

    add minimal pop/imaps scanner script

---
+++ popimaps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+TARGET='10.10.10.10'
+LOGFILE=$(date -u +'%Y%m%dT%H%M%SZ')'_'${TARGET//./_}'_popimaps.log'
+
+# Tiny friendly scanner: IMAPS first, then POP3S. Outputs appended to $LOGFILE.
+echo "== IMAPS scan: $(date -u +'%Y-%m-%dT%H:%M:%SZ')" >> "$LOGFILE"
+openssl s_client -connect "${TARGET}:imaps" -crlf 2>&1 >> "$LOGFILE"
+# u may want to check curl -k 'imaps://10.10.10.10' --user name:pw -v
+
+echo -e "\n== POP3S scan: $(date -u +'%Y-%m-%dT%H:%M:%SZ')" >> "$LOGFILE"
+openssl s_client -connect "${TARGET}:pop3s" -crlf 2>&1 >> "$LOGFILE"
+#  -starttls for STARTTLS
+
+
+echo "Scan complete. Results in $LOGFILE"
